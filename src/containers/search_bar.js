3b01d84c7351ff0130b0f9396ac35a48b0650486
kimbeomgyu <qjarb1994@gmail.com>
2019-04-28 15:02:06 +0900

    리액트와 폼 엘리먼트

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -9,12 +9,15 @@ class SearchBar extends Component {
     this.onInputChange = this.onInputChange.bind(this);
   }
   onInputChange(event) {
-    console.log(event.target.value);
     this.setState({ term: event.target.value });
   }
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
