a9c41556806ec77f3a345b1e644be1e3bae25a7f
kimbeomgyu <qjarb1994@gmail.com>
2019-04-28 14:54:23 +0900

    컨트롤된 컴포넌트와 컨텍스트 바인딩

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/search_bar';
 
-export default class App extends Component {
+class App extends Component {
   render() {
-    return <div>React simple starter</div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
   }
 }
+
+export default App;
