b06860fd19bca55fa1e9956b174c228ecb83964a
kimbeomgyu <qjarb1994@gmail.com>
2019-04-29 15:05:08 +0900

    단위 라벨링하기

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,12 +5,20 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const Humidity = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Chart data={temps} color="orange" />
+          <Chart data={temps} color="orange" units="K" />
+        </td>
+        <td>
+          <Chart data={pressures} color="green" units="hPa" />
+        </td>
+        <td>
+          <Chart data={Humidity} color="black" units="%" />
         </td>
       </tr>
     );
@@ -21,9 +29,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
